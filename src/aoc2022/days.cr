6895c361944ff3a4d2272992d93a02625086e0c6
Douglas Camata <159076+douglascamata@users.noreply.github.com>
2022-12-10 22:29:44 +0100

    Use macro to load day classes
    Signed-off-by: Douglas Camata <159076+douglascamata@users.noreply.github.com>

--- src/aoc2022/days.cr
+++ src/aoc2022/days.cr
@@ -1,15 +1,13 @@
 require "./days/*"
 
 module Aoc2022
-  DAYS = {
-    1 => Aoc2022::Day1,
-    2 => Aoc2022::Day2,
-    3 => Aoc2022::Day3,
-    4 => Aoc2022::Day4,
-    5 => Aoc2022::Day5,
-    6 => Aoc2022::Day6,
-    7 => Aoc2022::Day7,
-    8 => Aoc2022::Day8,
-    9 => Aoc2022::Day9,
+  {% begin %}
+    DAYS = {
+      {% for day, index in parse_type("Aoc2022").resolve.constants %}
+        {% if index != 0 %}
+          {{index}} => Aoc2022::{{day}},
+        {% end %}
+      {% end %}
   }
+  {% end %}
 end
