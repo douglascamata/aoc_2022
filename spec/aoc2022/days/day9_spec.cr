7270b20459815d180734e6d02b99f9a1e427b947
Douglas Camata <159076+douglascamata@users.noreply.github.com>
2022-12-10 21:01:46 +0100

    Add solution for day 9 part 2
    Signed-off-by: Douglas Camata <159076+douglascamata@users.noreply.github.com>

--- spec/aoc2022/days/day9_spec.cr
+++ spec/aoc2022/days/day9_spec.cr
@@ -3,27 +3,46 @@ require "../../../src/aoc2022/days"
 
 describe Aoc2022::Day9 do
   context "Given text input" do
-    input = Aoc2022::InMemoryInputReader.new(
-      <<-INPUT
-      R 4
-      U 4
-      L 3
-      D 1
-      R 4
-      D 1
-      L 5
-      R 2
-      INPUT
-    )
-    subject = Aoc2022::Day9.new(input)
-
     describe "#part1" do
+      input = Aoc2022::InMemoryInputReader.new(
+        <<-INPUT
+        R 4
+        U 4
+        L 3
+        D 1
+        R 4
+        D 1
+        L 5
+        R 2
+        INPUT
+      )
+      subject = Aoc2022::Day9.new(input)
+
       it "counts how many positions the tail visited" do
         subject.part1.should eq(13)
       end
     end
 
     describe "#part2" do
+      input = Aoc2022::InMemoryInputReader.new(
+        <<-INPUT
+        R 5
+        U 8
+        L 8
+        D 3
+        R 17
+        D 10
+        L 25
+        U 20
+        INPUT
+      )
+      subject = Aoc2022::Day9.new(input)
+
+      context "when the rope is longer" do
+        it "counts how many positions the tail visited" do
+          subject.part2.should eq(36)
+        end
+      end
     end
   end
 
@@ -38,6 +57,7 @@ describe Aoc2022::Day9 do
 
     describe "#part2" do
       it "gives correct result for my input", tags: "real" do
+        subject.part2.should eq(2665)
       end
     end
   end
--- src/aoc2022/days/day9.cr
+++ src/aoc2022/days/day9.cr
@@ -11,7 +11,13 @@ module Aoc2022
       rope.tail_moves.size
     end
 
-    def part2
+    def part2(debug = false)
+      rope = Rope.new(10, debug)
+      @input.split("\n").each do |raw_command|
+        raw_command, raw_count = raw_command.split(" ")
+        rope.move_head(Direction.parse(raw_command), raw_count.to_i)
+      end
+      rope.tail_moves.size
     end
 
     enum Direction
@@ -22,45 +28,65 @@ module Aoc2022
     end
 
     class Rope
+      alias Point = Tuple(Int32, Int32)
       getter tail_moves
-      @head : Tuple(Int32, Int32)
-      @tail : Tuple(Int32, Int32)
-      @tail_moves : Set(Tuple(Int32, Int32))
+      @tail_moves : Set(Point)
+      @debug : Bool
 
-      def initialize
-        @head = {0, 0}
-        @tail = {0, 0}
+      def initialize(size = 2, @debug = false)
+        @rope = Array(Point).new(size)
+        size.times { @rope << {0, 0} }
         @tail_moves = Set.new([{0, 0}])
       end
 
       def move_head(dir : Direction, count : Int32)
         count.times do
-          @head = move_point(@head, dir)
-          move_vector = {@head[0] - @tail[0], @head[1] - @tail[1]}
-          distance = move_vector.map(&.abs).sum
-          case {move_vector[0], move_vector[1]}
-          when {2, 0}
-            @tail = move_point(@tail, Direction::R)
-          when {-2, 0}
-            @tail = move_point(@tail, Direction::L)
-          when {0, 2}
-            @tail = move_point(@tail, Direction::U)
-          when {0, -2}
-            @tail = move_point(@tail, Direction::D)
-          when {2, 1}, {2, -1}
-            @tail = move_point(@head, Direction::L)
-          when {-2, 1}, {-2, -1}
-            @tail = move_point(@head, Direction::R)
-          when {1, 2}, {-1, 2}
-            @tail = move_point(@head, Direction::D)
-          when {1, -2}, {-1, -2}
-            @tail = move_point(@head, Direction::U)
+          @rope[0] = move_point(@rope[0], dir)
+          1.upto(@rope.size - 1) do |i|
+            process_move(@rope[i - 1], @rope[i]).try do |new_position|
+              @rope[i] = new_position
+            end
+            if i == @rope.size - 1
+              @tail_moves.add(@rope[-1])
+            end
           end
-          @tail_moves.add(@tail)
         end
       end
 
-      private def move_point(point : Tuple(Int32, Int32), dir : Direction)
+      private def process_move(moving : Point, previous : Point) : Point?
+        new_position : Point? = nil
+        move_vector = {moving[0] - previous[0], moving[1] - previous[1]}
+        distance = move_vector.map(&.abs).sum
+        case {move_vector[0], move_vector[1]}
+        when {2, 0}
+          new_position = move_point(previous, Direction::R)
+        when {-2, 0}
+          new_position = move_point(previous, Direction::L)
+        when {0, 2}
+          new_position = move_point(previous, Direction::U)
+        when {0, -2}
+          new_position = move_point(previous, Direction::D)
+        when {2, 2}
+          new_position = {previous[0] + 1, previous[1] + 1}
+        when {-2, 2}
+          new_position = {previous[0] - 1, previous[1] + 1}
+        when {2, -2}
+          new_position = {previous[0] + 1, previous[1] - 1}
+        when {-2, -2}
+          new_position = {previous[0] - 1, previous[1] - 1}
+        when {2, 1}, {2, -1}
+          new_position = move_point(moving, Direction::L)
+        when {-2, 1}, {-2, -1}
+          new_position = move_point(moving, Direction::R)
+        when {1, 2}, {-1, 2}
+          new_position = move_point(moving, Direction::D)
+        when {1, -2}, {-1, -2}
+          new_position = move_point(moving, Direction::U)
+        end
+        new_position
+      end
+
+      private def move_point(point : Point, dir : Direction) : Point
         case dir
         when Direction::U
           {point[0], point[1] + 1}
@@ -71,7 +97,7 @@ module Aoc2022
         when Direction::L
           {point[0] - 1, point[1]}
         else
-          {0, 0}
+          raise "Unknown direction"
         end
       end
     end
