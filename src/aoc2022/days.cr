e46f8e4e042e7e5761f77354af305b5e1a05742f
Douglas Camata <159076+douglascamata@users.noreply.github.com>
2022-12-11 23:44:16 +0100

    Fix day loader macro
    Signed-off-by: Douglas Camata <159076+douglascamata@users.noreply.github.com>

--- src/aoc2022/days.cr
+++ src/aoc2022/days.cr
@@ -5,7 +5,7 @@ module Aoc2022
     DAYS = {
       {% for day, index in parse_type("Aoc2022").resolve.constants %}
         {% if index != 0 %}
-          {{index}} => Aoc2022::{{day}},
+          {{day.stringify[3..].to_i}} => Aoc2022::{{day}},
         {% end %}
       {% end %}
   }
